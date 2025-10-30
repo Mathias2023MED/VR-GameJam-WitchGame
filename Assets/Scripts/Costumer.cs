65e1810ffa3eac088a9cd2913821a9a84b5b8a32
Mathias2023MED <145336123+Mathias2023MED@users.noreply.github.com>
2025-10-30 20:05:31 +0100

    Lidt animation script fix

--- Assets/Scripts/Costumer.cs
+++ Assets/Scripts/Costumer.cs
@@ -3,6 +3,20 @@ using System.Collections;
 
 public class Costumer : MonoBehaviour
 {
+    /* Calling Animation clips:
+    sapoAnimations.PlayDrink();
+    sapoAnimations.PlayShakingHead();
+    sapoAnimations.PlayDropKick(); 
+    
+    sapoAnimations.Walk1_Distance(2f);           2f = 2 meters     
+    sapoAnimations.Walk2_Distance(2f);
+    sapoAnimations.Walk3_Distance(2f);
+    sapoAnimations.Running_Distance(5f);              
+
+    sapoAnimations.WalkingOut_Distance(5f, useRoot: true, preTurnYawDeg: 40f, preTurnTime: 0.15f, restoreRotation: true, restoreTime: 0.15f);         
+    sapoAnimations.HurricaneKick_Distance(6f, true);  
+    */
+
     [Header("Requested Potion")]
     public PotionRecipeSO requestedPotion;
     public SapoAnimations sapoAnimations;
@@ -49,9 +63,9 @@ public class Costumer : MonoBehaviour
 
     private IEnumerator delay()
     {
-        yield return new WaitForSeconds(5f);
-        float walkDistance = 4f;
-        sapoAnimations.WalkingOut_Distance(walkDistance); // Call your "no" animation here
+        yield return new WaitForSeconds(4f);
+        sapoAnimations.WalkingOut_Distance(6f, useRoot: true, preTurnYawDeg: 40f, preTurnTime: 0.15f, restoreRotation: true, restoreTime: 0.15f);
+
     }
 
 }
