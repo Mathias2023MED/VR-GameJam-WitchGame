86a045ba486b2289fce001b454bfe52aff9dc269
SoffeSof <Sofie.d.andersen@hotmail.dk>
2025-11-06 12:16:44 +0100

    Bottle clicnking

--- Assets/Scripts/BreakableBottle.cs
+++ Assets/Scripts/BreakableBottle.cs
@@ -6,11 +6,17 @@ public class BreakableBottle : MonoBehaviour
     public GameObject brokenVersionDefault; // Prefab med smadret flaske (uden farve)
     public float breakForce = 5f;           // Hvor hårdt man skal ramme for at smadre
 
+    [Header("SOUND")]
+    [SerializeField] private AudioClip clinkClip;
+    [SerializeField] private AudioSource audioSource;
+    private float lastClinkTime;
+    private float clinkCooldown = 0.2f; // minimum 0.2 sek mellem klir
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.relativeVelocity.magnitude > breakForce)
+        float impact = collision.relativeVelocity.magnitude;
+        if (impact > breakForce)
         {
-            // 🔧 Use CompareTag("TagName"), not CompareTag == "TagName"
             if (CompareTag("EmptyBottle"))
             {
                 Instantiate(brokenVersionDefault, transform.position, transform.rotation);
@@ -21,6 +27,11 @@ public class BreakableBottle : MonoBehaviour
             }
 
             Destroy(gameObject); // Fjern den intakte flaske
+        }
+        else if (impact > 0.2f && Time.time - lastClinkTime > clinkCooldown)
+        {
+            lastClinkTime = Time.time;
+            SoundManager.Instance.PlaySound(audioSource, clinkClip);
         }
     }
 }
