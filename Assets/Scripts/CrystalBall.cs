384c39fbd0e259264824cb44b3dc077db5d3d5ff
Haku <stacia1402@gmail.com>
2025-11-07 23:21:20 +0100

    Crystall ball now has ads

---
+++ Assets/Scripts/CrystalBall.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.Video;
+using System.Collections;
+
+public class CrystalBall : MonoBehaviour
+{
+    public VideoPlayer idleVideo;
+    public VideoPlayer eventVideo;
+
+    public MeshRenderer idleRenderer;
+    public MeshRenderer eventRenderer;
+
+    public Collider triggerCollider;
+
+    public float minInterval = 10f;
+    public float maxInterval = 30f;
+
+    private bool isEventActive = false;
+    private bool isWaiting = false;
+
+    private void Awake()
+    {
+        triggerCollider.isTrigger = true;
+
+        // Start with idle
+        idleRenderer.enabled = true;
+        idleVideo.Play();
+
+        eventRenderer.enabled = false;
+        eventVideo.Pause();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RandomEventRoutine());
+    }
+
+    private IEnumerator RandomEventRoutine()
+    {
+        while (true)
+        {
+            if (!isEventActive && !isWaiting)
+            {
+                isWaiting = true;
+                float wait = Random.Range(minInterval, maxInterval);
+                yield return new WaitForSeconds(wait);
+                isWaiting = false;
+
+                if (!isEventActive)
+                    StartEventVideo();
+            }
+            yield return null;
+        }
+    }
+
+    private void StartEventVideo()
+    {
+        isEventActive = true;
+
+        // Pause idle, hide idle renderer
+        idleVideo.Pause();
+        idleRenderer.enabled = false;
+
+        // Show event renderer, play event
+        eventRenderer.enabled = true;
+        eventVideo.Play();
+    }
+
+    private void StopEventVideo()
+    {
+        if (!isEventActive) return;
+        isEventActive = false;
+
+        // Pause event, hide event renderer
+        eventVideo.Pause();
+        eventRenderer.enabled = false;
+
+        // Resume idle, show idle renderer
+        idleRenderer.enabled = true;
+        idleVideo.Play();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hand") && isEventActive)
+        {
+            StopEventVideo();
+        }
+    }
+}
